b07dd64345d2efcf51787cdb953dbf74a90eddb3
shivraj-roy <shivrajroy78@gmail.com>
2026-02-01 15:47:13 +0530

    feat: add Microsoft's clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -118,6 +118,18 @@ export default function RootLayout({
                   gtag('config', 'G-DBW3C7ZH6S');
                `}
             </Script>
+
+            {/* Microsoft Clarity */}
+            <Script id="microsoft-clarity" strategy="afterInteractive">
+               {`
+                  (function(c,l,a,r,i,t,y){
+                     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                  })(window, document, "clarity", "script", "vaimrre0nh");
+               `}
+            </Script>
+
             {children}
             <Analytics />
          </body>
